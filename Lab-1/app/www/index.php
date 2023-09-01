3c9a456bc64245a4ae89b58df6751bc4d43c021d
Bastian Ignacio Rojas Pereira <64908739+Bastian-Rojas@users.noreply.github.com>
2023-09-01 10:23:21 -0400

    Add files via upload
    Laboratorio 1 Listo, Index completo

--- Lab-1/app/www/index.php
+++ Lab-1/app/www/index.php
@@ -1,19 +1,52 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["nombre"];
+    $last_name = $_POST["apellido"];
+    if (empty($name) || empty($last_name)) {
+        echo "Faltan datos";
+    } else {
+        $conn = mysqli_connect('db', 'root', 'test', 'dbname');
+        if (!$conn) {
+            die("Error al conectar a la base de datos: " . mysqli_connect_error());
+        }
+        $query_id = 'SELECT MAX(id) AS id FROM Names;';
+        $result = mysqli_query($conn, $query_id);
+        if ($result) {
+            $id = ($result->fetch_object()->id) + 1;
+            $query = "INSERT INTO Names VALUES (?, ?, ?)";
+            $stmt = mysqli_prepare($conn, $query);
+            mysqli_stmt_bind_param($stmt, 'iss', $id, $name, $last_name);
+            
+            if (mysqli_stmt_execute($stmt)) {
+                echo "Registro agregado correctamente";
+            } else {
+                echo "Error al agregar el registro: " . mysqli_error($conn);
+            }
+            
+            mysqli_stmt_close($stmt);
+        } else {
+            echo "Error al obtener el ID máximo: " . mysqli_error($conn);
+        }
 
-<html>
-    <head>
-        <title>Aplicaciones de Internet</title>
-        <meta charset="utf-8">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-    </head>
-    <body>
-        <div class="container-fluid">
-            <div class="row">
-                <h1>Aplicaciones de Internet</h1>
-                <table class="table table-striped">
-                    <thead>
-                        <tr>
-                            <th></th>
+        mysqli_close($conn);
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>Aplicaciones de Internet</title>
+    <meta charset="utf-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <h1>Aplicaciones de Internet</h1>
+            <table class="table table-striped">
+                <thead>
+                <th></th>
                             <th>id</th>
                             <th>Nombre</th>
                             <th>Apellido</th>
@@ -23,7 +56,7 @@
 
                         $conn = mysqli_connect('db', 'root', 'test', "dbname");
 
-                        $query = 'SELECT * From Person';
+                        $query = 'SELECT * From Names';
                         $result = mysqli_query($conn, $query);
 
                         while($value = $result->fetch_array(MYSQLI_ASSOC)){
@@ -42,5 +75,17 @@
                     </table>
             </div>
         </div>
-    </body>
+        <div class="row">
+            <form method="POST" action="index.php">
+                <div class="mb-3">
+                    <label class="form-label">Nombre</label>
+                    <input type="text" class="form-control" name="nombre">
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Apellido</label>
+                    <input type="text" class="form-control" name="apellido">
+                </div>
+                <button type="submit" class="btn btn-primary">Agregar</button>
+            </form>
+</body>
 </html>
