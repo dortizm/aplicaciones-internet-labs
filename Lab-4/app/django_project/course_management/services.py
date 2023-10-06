8e2106d4e0839f6324f88b6e405256cc37d6ae60
HendrikC0413 <80849156+HendrikC0413@users.noreply.github.com>
2023-10-06 12:39:05 -0300

    Se finaliza laboratorio 4.

--- Lab-4/app/django_project/course_management/services.py
+++ Lab-4/app/django_project/course_management/services.py
@@ -1,14 +1,9 @@
 import requests
 
 def generate_request():
-    url = "https://moviesdatabase.p.rapidapi.com/titles/random"
-    querystring = {"list":"most_pop_movies"}
-    headers = {
-	"X-RapidAPI-Key": "9937e1efffmsh697d9382ff77e08p16a91bjsn5a45b3d0ddf1",
-	"X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com"
-        }
+    url = "http://api.filmon.com/api/vod/search?genre=action"
     
-    response = requests.get(url, headers=headers, params=querystring)
+    response = requests.get(url)
 
     if response.status_code == 200:
-        return response.json()
+        return response.json()['response']
