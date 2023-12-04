97105df762f9bc11a64d6b299d1d1a010928586a
MatiasAstudillo <64876967+MatiasAstudillo@users.noreply.github.com>
2023-12-04 10:31:31 -0300

    Delete services.py

--- services.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-def generate_request():
-    url = 'http://api.filmon.com/api/vod/genres'
-    response = requests.get(url)
-    
-
-    if response.status_code == 200:
-        return response.json()['response']
